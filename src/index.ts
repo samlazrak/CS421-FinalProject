4b647878d3a542e92a2db50ff241a132de6ec072
Sam Lazrak <sam@lazrak.me>
2018-06-27 14:04:09 -0500

    crappy fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import Database from "./database/Database"
+/* import Database from "./database/Database" */
 import app from "./app"
 
-const config = {
+/* const config = {
   host: "localhost:3306",
   user: "root",
   password: "foobar",
   database: "test"
 }
 
-const database = new Database(config)
+const database = new Database(config) */
 
 module.exports = app.listen(app.get("port"), () => {
   console.log("🚀  " + "Server listening on port " + app.get("port") + " 📬")
