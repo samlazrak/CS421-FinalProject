0223d9523514223fabf5aaa7510fb2f24a673778
JPhan21 <40611432+JPhan21@users.noreply.github.com>
2018-07-25 13:36:55 -0500

    Create login.component.ts

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { Router } from "@angular/router";
+import "rxjs/Rx";
+ 
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+ 
+    public input: any;
+ 
+    constructor(private http: Http, private router: Router) {
+        this.input = {
+            "email": "",
+            "password": ""
+        };
+    }
+ 
+    public login() {
+        if(this.input.email && this.input.password) {
+            let headers = new Headers({ "content-type": "application/json" });
+            let options = new RequestOptions({ headers: headers });
+            this.http.post("http://localhost:4200/login", JSON.stringify(this.input), options)
+                .map(result => result.json())
+                .subscribe(result => {
+                    this.router.navigate(["/main"], { "queryParams": result });
+                });
+        }
+    }
+ 
+}
