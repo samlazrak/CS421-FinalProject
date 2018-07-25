0a5aaa7b7aad0679174c44d798d7a086677d4f2b
Engion <engulge@gmail.com>
2018-07-25 15:57:22 -0500

    fix to compile issues

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -15,13 +15,10 @@ export const UserSchema = new Schema({
     type: String,
     required: "Enter a name"
   },
-<<<<<<< HEAD
-=======
   password: {
     type: String,
     required: true
   },
->>>>>>> sql
   created_date: {
     type: Date,
     default: Date.now
