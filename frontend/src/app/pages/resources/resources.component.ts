3a723c56e3ef386dbe0ae01b4cea600d38aaba8c
RyanHardin <smso3223@uab.edu>
2018-08-06 15:55:24 -0500

    Made changes to the resources page

--- frontend/src/app/pages/resources/resources.component.ts
+++ frontend/src/app/pages/resources/resources.component.ts
@@ -17,6 +17,54 @@ export class ResourcesComponent implements OnInit {
     this.resources$ = this.resourceService.resources();
   }
 
+//This creates a new post for the user
+newPanel(){
+
+var div = document.createElement('div');
+div.className = 'container';
+var divRow = document.createElement('div');
+divRow.className = 'row';
+divRow.style.margin = "20px";
+var divColImg = document.createElement('div');
+divColImg.className = 'col col-sm-2';
+
+//This is only here to outline users profile picture
+divColImg.style.border = "5px solid grey";
+
+var panel = document.createElement('div');
+panel.className = 'col col-lg-14';
+var card = document.createElement('div');
+card.className = 'card';
+var cardHeader = document.createElement('div');
+cardHeader.className = 'card-header';
+cardHeader.innerHTML = 'Featured';
+var cardBody = document.createElement('div');
+cardBody.className = 'card-body';
+var innerDiv = document.createElement('div');
+var header = document.createElement('h5');
+header.className = 'card-title';
+header.innerHTML = "Special title treatment";
+var comment = document.createElement('p');
+comment.className = 'card-text';
+comment.innerHTML = 'With supporting text below as a natural lead-in to additional content.';
+var link = document.createElement('a');
+link.className = 'btn btn-primary';
+link.href = '#';
+link.innerHTML = "Go Somewhere";
+
+div.appendChild(divRow);
+divRow.appendChild(divColImg);
+divRow.appendChild(panel);
+panel.appendChild(card);
+card.appendChild(cardHeader);
+card.appendChild(cardBody);
+cardBody.appendChild(innerDiv);
+innerDiv.appendChild(header);
+innerDiv.appendChild(comment);
+innerDiv.appendChild(link);
+
+document.body.appendChild(div);
+}
   ngOnInit() {
   }
 
