5beb843af7b710856427d96e19721efa6a65132c
JPhan21 <40611432+JPhan21@users.noreply.github.com>
2018-08-07 11:52:34 -0500

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,15 +5,19 @@ import { ResourcesComponent } from './pages/resources/resources.component';
 import { ResourceComponent } from './core/components/resource/resource.component';
 import { MainComponent} from './pages/main/main.component';
 import { CreateProfileComponent } from './pages/create-profile/create-profile.component';
+import { LoginProfileComponent } from './pages/login-profile/login-profile.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'main', component: MainComponent },
+  { path: 'about', component: AboutComponent},
   { path: 'resources', component: ResourcesComponent },
   { path: 'resources/:id', component: ResourceComponent },
-  {path: 'createProfile', component: CreateProfileComponent},
-  {path: 'userProfile', component: UserProfileComponent}
+  { path: 'createProfile', component: CreateProfileComponent },
+  { path: 'loginProfile', component: LoginProfileComponent },
+  { path: 'userProfile', component: UserProfileComponent }
 ];
 
 @NgModule({
@@ -25,4 +29,4 @@ const routes: Routes = [
   ],
 })
 export class AppRoutingModule { }
-export const routingComponents = [MainComponent, ResourceComponent, ResourcesComponent, CreateProfileComponent, UserProfileComponent];
+export const routingComponents = [MainComponent, AboutComponent, ResourceComponent, ResourcesComponent, CreateProfileComponent, LoginProfileComponent, UserProfileComponent];
