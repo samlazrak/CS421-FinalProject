c5b88e18aa0cc556585143592a719f001d8e94e3
RyanHardin <smso3223@uab.edu>
2018-08-09 08:09:09 -0500

    changes for presentation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { MainComponent} from './pages/main/main.component';
 import { CreateProfileComponent } from './pages/create-profile/create-profile.component';
 import { LoginProfileComponent } from './pages/login-profile/login-profile.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import {AboutComponent} from './pages/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path: 'resources/:id', component: ResourceComponent },
   { path: 'createProfile', component: CreateProfileComponent },
   { path: 'loginProfile', component: LoginProfileComponent },
-  { path: 'userProfile', component: UserProfileComponent }
+  { path: 'userProfile', component: UserProfileComponent },
+  {path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
@@ -27,4 +29,4 @@ const routes: Routes = [
   ],
 })
 export class AppRoutingModule { }
-export const routingComponents = [MainComponent, ResourceComponent, ResourcesComponent, CreateProfileComponent, LoginProfileComponent, UserProfileComponent];
+export const routingComponents = [MainComponent, ResourceComponent, ResourcesComponent, CreateProfileComponent, LoginProfileComponent, UserProfileComponent, AboutComponent];
